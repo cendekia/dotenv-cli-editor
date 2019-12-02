1b8feadcd59380a0fce7f75b8ae07038fb1f349f
cendekia <me@cendekiapp.com>
2019-12-02 08:58:29 +0700

    add mocha and chai for unit testing

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+const expect = require('chai').expect;
+
+
+describe('dotenv-cli edit', () => {
+    it('this test should be replaced with real tests', () => {
+        expect(true);
+    })
+})
